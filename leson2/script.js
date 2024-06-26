53a4857e207f58849b22714f3b5d3933314e00c5
MychailoM <142823203+MychailoM@users.noreply.github.com>
2024-06-26 21:24:17 +0300

    leson 2

---
+++ leson2/script.js
@@ -0,0 +1,88 @@
+// "use strict";
+// const a = 64;
+// const b = 32;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// console.log(a < b);
+// console.log(a > b);
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a === b);
+
+// const d = "dwa";
+// console.log(typeof Number(d));
+
+// console.log(Number.parseInt("15px24")); //10
+// console.log(Number.parseInt('10.5px')); //10
+// console.log(Number.parseInt("15px24")); //15
+// console.log(Number.parseInt("px")); //NaN
+
+
+// console.log(Number.parsefloat("15.5px24")); //15.5
+// console.log(Number.parsefloat('10.5px')); //10.5
+// console.log(Number.parsefloat("15.5px24")); //15.5
+// console.log(Number.parsefloat("px")); //NaN
+
+
+// const x = Number('25');
+// console.log(Number.isNaN(x));//false
+
+// const y = Number("dwa");
+// console.log(Number.isNaN(y));//true
+
+// console.log(0.1 + 0.3 === 0.4);
+// console.log(0.1 + 0.3);
+
+// let r = (0.2 * 10 + 0.3 * 10) / 10;
+// console.log(r);
+
+// console.log(Math.floor(2.8)); //2
+// console.log(Math.ceil(2.8)); //3
+// // округлює до ближчого цілого числа
+// console.log(Math.round(2.5)); //3
+// console.log(Math.round(2.8)); //3
+// console.log(Math.round(2.3)); //2
+
+// console.log(Math.max(2.3, 375, 4, 99)); //375
+// console.log(Math.min(2.3, 375, 4, 99)); //2.3
+
+// console.log(Math.pow(5, 3)); //125
+
+// console.log(Math.random() * (10 - 1) + 1); //
+
+
+
+// let a1 = 5 + 3;
+// let a2 = 5 - 3;
+// let a3 = 5 * 3;
+// let a4 = 5 / 3;
+// let a5 = 5 % 3;
+// console.log(a1, a2, a3, a4, a5);
+
+
+
+// let h = 23;
+// let w = 10;
+// console.log(h * w , 'см');
+
+
+
+// console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+// console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+
+
+
+// const k1 = Number.parseFloat('5px');
+// const k3 = Number.parseFloat("12djd334");
+// const k2 = Number.parseFloat("12.45asdwe12");
+// const k4 = Number.parseFloat("qwqweeewq");
+// console.log(k1, k2, k3, k4)
+
+
