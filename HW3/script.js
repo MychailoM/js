3af9883c7b07e0c34228dc6b24c66e849f20bbc1
MychailoM <142823203+MychailoM@users.noreply.github.com>
2024-07-04 17:58:27 +0300

    HW 3

---
+++ HW3/script.js
@@ -0,0 +1,16 @@
+const result = 5 + 5 + '5'; 
+console.log(result);
+
+const email = 'marsmisha444@gmail.com';
+console.log(email.includes('@'));
+console.log(email.length);
+
+const my = 'My';
+const name = 'name';
+const is = "is";
+const fullName = `${my} ${name} ${is} Mykhailo`; 
+console.log(fullName);
+
+const userName = 'Олеже';
+const payment = 159;
+alert( `Дякуємо ${userName}! До сплати ${payment} гривень.` )
