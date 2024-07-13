8bd5cf72a00d79ffbf156646ff35bd040820656f
MychailoM <142823203+MychailoM@users.noreply.github.com>
2024-07-13 13:55:51 +0300

    lesson 4

---
+++ lesson4/script.js
@@ -0,0 +1,142 @@
+//&& - і
+
+// const num = 25;
+// const res = num > 15 && num < 35;
+// console.log(res)
+
+// const numer = 45;
+// const result = numer > 15 && numer < 35;
+// console.log(result);
+
+
+
+//|| - Або
+
+// const num = 25;
+// const res = num > 15 || num < 35;
+// console.log(res)
+
+// const numer = 45;
+// const result = numer > 15 || numer < 35;
+// console.log(result);
+
+// const nume = 45;
+// const resul = numer > 55 || numer < 35;
+// console.log(resul);
+
+
+
+//! - не
+// console.log(!true);
+// console.log(!false);
+// console.log(!0);
+// console.log(!1);
+
+
+
+//if
+// let cost = 0;
+// const sub = 'premium';
+
+// if (sub === 'premium') {
+//     cost = 100;
+// }
+// console.log(cost);
+
+//else
+
+// let cost;
+// const sub = 'pro';
+
+// if (sub === 'premium') {
+//     cost = 100;
+// } else {
+//     cost = 0;
+// }
+// console.log(cost);
+
+//elif
+// let cost;
+// const sub = 'pro';
+
+// if (sub === 'free') {
+//     cost = 0;
+// } else if (sub === 'pro') {
+//     cost = 100;
+// } else if (sub === 'premium') {
+//     cost = 250;
+// } else {
+//     sub = 'Ви не підписані';
+// }
+
+// console.log(cost);
+
+
+
+
+//ternar operator
+
+// let type;
+// const age = 15;
+
+// if (age >= 18) {
+//     type = 'abult';
+// } else {
+//     type = 'child';
+// }
+// console.log(type);
+
+
+
+// const age = 15;
+// const type = age >= 18 ? "abult" : "child";
+// console.log(type);
+
+
+
+
+
+// let question = prompt('Яка “офіційна” назва JavaScript?');
+// if (question === 'ECMAScript') {
+//     alert('Правильно!')
+// } else {
+//     alert('Ви не знаєте? ECMAScript!')
+// };
+
+// const x1 = 10;
+// const x2 = 30;
+// const number = 45;
+
+// if (x1 < number) {
+//   console.log("до x1");
+// } else if (x1 > number) {
+//   console.log("після x1");
+// } else if (x1 <= number && x2 >= number) {
+//   console.log("від x1 до x2");
+// } else (x1 >= number || x2 <= number) {
+//   console.log("до х1 або після х2");
+// };
+
+// const isOnline = false;
+// const isFriend = true;
+// const isDnd = false;
+
+// if (isFriend && isOnline && !isDnd) {
+//     console.log('можна відкрити чат');
+// } else {
+//     console.log('Не можна відкрити чат');
+// }
+
+
+let sub;
+if (sub === "pro" || sub === "vip") {
+    console.log('дозвіл отримано');
+} else {
+    console.log('дозвіл не отримано');
+};
+
+
+
+
+
+
