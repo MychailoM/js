aad8224cd0da38b7c0c11a7dc25fbfb601face4a
MychailoM <142823203+MychailoM@users.noreply.github.com>
2024-08-14 10:53:00 +0300

    wda

---
+++ lesson9/script.js
@@ -0,0 +1,42 @@
+// const add = function (a, b, c) {
+//     return a + b + c;
+// }
+
+// const result = add(1, 2, 3);
+// console.log(result);
+
+// // АБО
+
+// console.log(add(1, 2, 3));
+
+
+
+// const count = function (countFrom = 1, countTo = 10, step = 1) {
+//     console.log(`починаємо з ${countFrom} і закінчуємо ${countTo}`);
+//     for (let i = countFrom; i <= countTo; i += step){
+//         console.log(i);
+//     }
+// }
+// count(2, 7);
+
+
+// console.log(addNumbers(5, 4, 3))
+// function addNumbers(a, b, c) {
+//     return a + b + c;
+// }
+
+// console.log(addNumbers(1, 2, 3))
+
+
+
+// const sum = function () {
+//     let total = 0;
+//     for (const argument of arguments) {
+//         total += argument;
+//     }
+//     return total;
+//     const args = Array.from(arguments)
+// }
+// console.log(sum(5, 3, 4))
+// console.log(sum(10, 15, 20, 30, 50));
+
