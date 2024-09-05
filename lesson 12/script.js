a5550a675d4fac92bf849918f21911e595575e81
MychailoM <142823203+MychailoM@users.noreply.github.com>
2024-09-05 18:48:14 +0300

    w

---
+++ lesson 12/script.js
@@ -0,0 +1,64 @@
+// const hotel = {
+//     name: 'hilton',
+//     stars: 5,
+//     capacity: 150,
+// }
+
+//виводить ключі
+// for (const key in hotel) {
+//     console.log('key:', key)
+// }
+
+//виводить властивості
+// for (const key in hotel) {
+//     console.log('value:', hotel[key])
+// }
+
+
+// for (const key in hotel) {
+//     console.log(`${key}, ${hotel[key]}`);
+// }
+
+
+// const keys = Object.keys(hotel);
+// console.log(keys);
+
+// const values = Object.values(hotel);
+// console.log(values);
+
+// const entries = Object.entries(hotel);
+// console.log(entries);
+
+
+// const hotel = {
+//     name: "hilton",
+//     stars: 5,
+//     capacity: 150,
+// };
+
+// const keys = Object.keys(hotel);
+// for (const key of keys) {
+//     console.log(hotel[key])
+// }
+
+// const entries = Object.entries(hotel);
+// for (const entry of entries) {
+//     const key = entry[0];
+//     const value = entry[1];
+//     console.log(key, value)
+// }
+
+
+// const foods = {
+//     orange: 5, 
+//     kivi: 12,
+//     banana: 79,
+//     apple: 79.5,
+// }
+// const values = Object.values(foods);
+// let total = 0;
+// for (const value of values) {
+//     total += value
+// }
+// console.log(total)
+
